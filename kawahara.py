35f02f502bc2b168138ddf9a35d3e75a9af434f0
Florian Bogner <e1225415@student.tuwien.ac.at>
2024-04-04 16:38:10 +0200

    DFS für die BDDs. alternate und unalternate für die strings

--- kawahara.py
+++ kawahara.py
@@ -1,4 +1,5 @@
 from bidict import bidict
+from itertools import starmap
 
 
 Bottom = False
@@ -47,7 +48,7 @@ def Algo_1(n: int) -> BBD_node:
             add_to_bidict(state, R_state, unprocessed_nodes)
             v.R = state.inverse[R_state]
 
-    print(state)
+    #print(state)
 
     return root
 
@@ -90,5 +91,51 @@ def add_to_bidict(the_bidict, key, the_list):
         the_bidict[node] = key
         the_list.append(node)
 
+def BBD_DFS(root: BBD_node) -> list:
+    out = []
+    current_string = []
+
+    
+    def recurse(node: BBD_node):
+        if node.is_zero_terminal:
+            return
+        if node.is_one_terminal:
+            out.append("".join(current_string))
+            return
+        current_string.append("L")
+        recurse(node.L)
+        current_string.pop()
+        current_string.append("R")
+        recurse(node.R)
+        current_string.pop()
+
+    recurse(root)
+
+    return out
+
+def invert(s: str) -> str:
+    s = s.replace("R", "X")
+    s = s.replace("L", "R")
+    s = s.replace("X", "L")
+    return s
+
+# abcdef -> afbecd
+def alternate(s: str) -> str:
+    n = len(s)//2
+    return "".join(starmap(lambda a,b:a+b, zip(s[:n], s[:n-1:-1])))
+    # the uglier the code the better lol
+
+# abcdef -> acefdb
+def unalternate(s: str) -> str:
+    return s[0::2] + s[-1::-2]
+
+    
+
+
 if __name__ == "__main__":
-    Algo_1(2)
+    strings = BBD_DFS(Algo_1(3))
+    # print(alternate("abcdef"))
+
+    for s in strings:
+        print(s, "->", unalternate(s), "->", ss := alternate(unalternate(s)))
+        assert s == ss
